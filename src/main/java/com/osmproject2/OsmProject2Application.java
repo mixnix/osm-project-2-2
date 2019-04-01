25a1c65ec61085e445c1fbe5dffa82c4a6ff6f15
Michał <mixnix@users.noreply.github.com>
2019-04-01 14:04:08 +0200

    first commit

---
+++ src/main/java/com/osmproject2/OsmProject2Application.java
@@ -0,0 +1,13 @@
+package com.osmproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmProject2Application.class, args);
+	}
+
+}
