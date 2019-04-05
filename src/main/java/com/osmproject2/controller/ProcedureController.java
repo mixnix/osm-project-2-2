30f98b7a43b8fc59156114ab20a5f659d7c9f990
Michał <mixnix@users.noreply.github.com>
2019-04-05 16:54:01 +0200

    listing procedures works

--- src/main/java/com/osmproject2/controller/ProcedureController.java
+++ src/main/java/com/osmproject2/controller/ProcedureController.java
@@ -1,18 +1,21 @@
 package com.osmproject2.controller;
 
 
+import com.osmproject2.entity.Patient;
 import com.osmproject2.entity.Procedure;
 import com.osmproject2.repository.PatientRepository;
 import com.osmproject2.repository.ProcedureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 public class ProcedureController {
 
     @Autowired
@@ -22,9 +25,15 @@ public class ProcedureController {
     private PatientRepository patientRepository;
 
     @GetMapping("/patient/{patientId}/procedure")
-    public List<Procedure> getAllProceduresByPatientId(@PathVariable (value = "patientId") Integer patientId){
-        return procedureRepository.findByPatientId(patientId);
+    public String listProcedures(@PathVariable (value = "patientId") Integer patientId,
+                                                       Model model){
+        List<Procedure> procedures = procedureRepository.findByPatientId(patientId);
+
+        model.addAttribute("procedures", procedures);
+
+        return "list-procedures";
     }
+
 
 //    @PostMapping("/patients/{patientId}/procedures")
 //    public Procedure createProcedure(@PathVariable (value = "patientId") Integer patientId,
